0158fa779a9b5ce908aa66cbfe89cf9452edf660
Yuhui <mrhuangyuhui@users.noreply.github.com>
2021-06-04 10:26:11 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ curl -fsSL https://get.docker.com -o get-docker.sh \
 # 安装 Outline Server
 sudo bash -c "$(wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh)"
 
-# 设置 2G 流量限制
+# 设置 5G 流量限制
 API_URL=$(sudo grep -Eo 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)' /opt/outline/access.txt) \
-&& curl -v --insecure -X PUT -H "Content-Type: application/json" -d '{"limit": {"bytes": 2000000000}}' $API_URL/access-keys/0/data-limit \
+&& curl -v --insecure -X PUT -H "Content-Type: application/json" -d '{"limit": {"bytes": 5000000000}}' $API_URL/access-keys/0/data-limit \
 && curl --insecure $API_URL/access-keys/ | egrep -o 'ss://.*\?outline=1'
